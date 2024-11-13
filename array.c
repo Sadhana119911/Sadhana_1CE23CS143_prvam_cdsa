712870052aca33af7f56e42352c84d844e0b92a7
Sadhana119911 <sadhanahiremath11@gmail.com>
2024-11-13 14:21:57 +0530

    Add files via upload

--- array.c
+++ array.c
@@ -1,21 +1,24 @@
 #include<stdio.h>
-int isPresent(int n,int arr[],int target){
-    for(int i=0;i<n;i++){
-        if(arr[i]==target){
-            return 1;
-        }
-    }
-    return 0;
-}
 int main(){
     int n;
+    printf("Enter the size");
     scanf("%d",&n);
     int arr[n];
+    printf("Enter the elements");
     for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+        scanf("%d\n",&arr[i]);
     }
-    int target;
-    scanf("%d",&target);
-    printf("%d",isPresent(n,arr,target));
+    int min1=__INT_MAX__;
+    int min2=__INT_MAX__;
+    for(int i=0;i<n;i++){
+        if(arr[i]<min1){
+            min2=min1;
+            min1=arr[i];
+        }
+        else if(arr[i]<min2){
+            min2=arr[i];
+        }
+    }
+    printf("The second minimum is:");
+    printf("%d",min2);
 }
- 
